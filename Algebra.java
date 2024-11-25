cd36085161e14e84a02499aafd64ba1f773ab708
yarden raab <yarden.raab@gmail.com>
2024-11-25 18:45:49 +0200

    Algebraic operations 3

--- Algebra.java
+++ Algebra.java
@@ -7,14 +7,14 @@ public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
 	    // System.out.println(plus(2,-3));   // 2 + 3
-	    //System.out.println(minus(7,-2));  // 7 - 2
-   		//System.out.println(minus(-2,7));  // 2 - 7
- 		System.out.println(times(3,-2));  // 3 * 4
+	    // System.out.println(minus(7,-2));  // 7 - 2
+   		// System.out.println(minus(-2,7));  // 2 - 7
+ 		// System.out.println(times(3,-2));  // 3 * 4
    		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		// System.out.println(pow(5,3));      // 5^3
    		// System.out.println(pow(3,5));      // 3^5
    		// System.out.println(div(12,3));   // 12 / 3    
-   		// System.out.println(div(5,5));    // 5 / 5  
+   		System.out.println(div(10, -5));    // 5 / 5  
    		// System.out.println(div(25,7));   // 25 / 7
    		// System.out.println(mod(25,7));   // 25 % 7
    		// System.out.println(mod(120,6));  // 120 % 6    
@@ -80,10 +80,27 @@ public class Algebra {
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		int count =0;
-		while (x1>=x2) {
-			x1 = minus(x1, x2);
+		int count = 0;
+		int temp1 = 0;
+		if (x1>=0) {
+			temp1 = x1;
+		}
+		else {
+			temp1 = -x1;
+		}
+		int temp2 = 0;
+		if (x2>=0) {
+			temp2 = x2;
+		}
+		else {
+			temp2 = -x2;
+		}
+		while (temp1>=temp2) {
+			temp1 = minus(temp1, temp2);
 			count++;
+		}
+		if ((x1>=0 && x2<0) || (x2>=0 && x2<1)) {
+			return -count;
 		}
 		return count;
 	}
